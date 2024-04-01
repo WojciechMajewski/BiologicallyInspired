e3070bf139b9071c80bcd4c76763f0eb0773c93b
TwistedTaleTeller <101590286+WojciechMajewski@users.noreply.github.com>
2024-04-01 02:33:59 +0200

    Buggy greedy
    also fix randomness

--- BiologicallyInspired.cpp
+++ BiologicallyInspired.cpp
@@ -1,12 +1,12 @@
 #include "Utility.h"
 
 int main() {
-    //std::srand(148253);
+    std::srand(148253);
 
     auto rng = std::default_random_engine{};
 
     std::string filename = "ATSP/br17.atsp";
-    //filename = "ATSP/ft70.atsp";
+    //filename = "ATSP/ftv35.atsp";
 
     std::vector <std::vector <int>> edge_matrix = read_file(filename);
 
@@ -24,7 +24,19 @@ int main() {
 
 
 
+
+    solution = solution_search(5, "greedy", edge_matrix, rng);
+
+    std::cout << "Greedy searched solution length: " << calculate_distance(solution, edge_matrix) << "\n";
+
+
+
     solution = solution_search(5, "steepest", edge_matrix, rng);
+
+    std::cout << "Steepest searched solution length: " << calculate_distance(solution, edge_matrix) << "\n";
+
+
+    solution = solution_search(25, "steepest", edge_matrix, rng);
 
     std::cout << "Steepest searched solution length: " << calculate_distance(solution, edge_matrix) << "\n";
 
--- Utility.cpp
+++ Utility.cpp
@@ -90,10 +90,7 @@ std::vector <int> random_solution(int dimension, std::default_random_engine& rng
 std::vector <int> steepest_solution(std::vector <int> solution, std::vector <std::vector <int>>& edge_matrix) {
     // if not steepest_neighborhood, then greedy
     // if not edges_exchange, then nodes exchange // But intra- and inter- should both be used
-
-    /// FOR NOW, STEEPEST:
-    bool steepest_neighborhood = true;
-    
+       
 
 
     // 100 iterations instead of while(true)
@@ -108,122 +105,147 @@ std::vector <int> steepest_solution(std::vector <int> solution, std::vector <std
         int first_to_switch_index = -1;
         int second_to_switch_index = -1;
 
-        if (steepest_neighborhood) {
-            // Then edge/node reordering
-            // Two pairs of consecutive nodes
-            for (int i = 0; i < solution.size(); i++) {
-                for (int j = 0; (j + 1) % solution.size() < i; j++) {
-                    if (j == i || j == (i + 1) % solution.size() || (j + 1) % solution.size() == i) continue; // catch intersections
-                    int old_cost = 0;
-                    int cost_improvement = 0;
+        // Then edge/node reordering
+        // Two pairs of consecutive nodes
+        for (int i = 0; i < solution.size(); i++) {
+            for (int j = 0; (j + 1) % solution.size() < i; j++) {
+                if (j == i || j == (i + 1) % solution.size() || (j + 1) % solution.size() == i) continue; // catch intersections
+                int old_cost = 0;
+                int cost_improvement = 0;
 
-                    old_cost += get_cost(solution[i], solution[(i + 1) % solution.size()], edge_matrix);
-                    old_cost += get_cost(solution[j], solution[(j + 1) % solution.size()], edge_matrix);
-
-
-                    //cost_improvement -= get_cost(solution[i], solution[(j+1) % solution.size()], edge_matrix);
-                    //cost_improvement -= get_cost(solution[j], solution[(i+1) % solution.size()], edge_matrix);
-
-                    cost_improvement -= get_cost(solution[(j + 1) % solution.size()], solution[(i + 1) % solution.size()], edge_matrix);
-                    cost_improvement -= get_cost(solution[j], solution[i], edge_matrix);
+                old_cost += get_cost(solution[i], solution[(i + 1) % solution.size()], edge_matrix);
+                old_cost += get_cost(solution[j], solution[(j + 1) % solution.size()], edge_matrix);
 
 
-                    cost_improvement += old_cost;
+                //cost_improvement -= get_cost(solution[i], solution[(j+1) % solution.size()], edge_matrix);
+                //cost_improvement -= get_cost(solution[j], solution[(i+1) % solution.size()], edge_matrix);
 
-                    if (cost_improvement > best_improvement) {
-                        best_improvement = cost_improvement;
+                cost_improvement -= get_cost(solution[(j + 1) % solution.size()], solution[(i + 1) % solution.size()], edge_matrix);
+                cost_improvement -= get_cost(solution[j], solution[i], edge_matrix);
 
-                        first_edge_start_index = j;
-                        second_edge_start_index = i; // As j is always smaller than i
 
-                        new_node_index = -1;
-                    }
+                cost_improvement += old_cost;
+
+                if (cost_improvement > best_improvement) {
+                    best_improvement = cost_improvement;
+
+                    first_edge_start_index = j;
+                    second_edge_start_index = i; // As j is always smaller than i
+
+                    new_node_index = -1;
                 }
             }
+        }
 
             
-            if (best_improvement > 0) {
+        if (best_improvement > 0) {
                 
-                if (((first_edge_start_index + 1) % solution.size()) > ((second_edge_start_index + 1) % solution.size())) {
-                    int temp = first_edge_start_index;
-                    first_edge_start_index = second_edge_start_index;
-                    second_edge_start_index = temp;
-                }
-                // Edge exchange through flipping a subpath
-                std::reverse(solution.begin() + ((first_edge_start_index + 1) % solution.size()), solution.begin() + ((second_edge_start_index + 1) % solution.size()));
+            if (((first_edge_start_index + 1) % solution.size()) > ((second_edge_start_index + 1) % solution.size())) {
+                int temp = first_edge_start_index;
+                first_edge_start_index = second_edge_start_index;
+                second_edge_start_index = temp;
+            }
+            // Edge exchange through flipping a subpath
+            std::reverse(solution.begin() + ((first_edge_start_index + 1) % solution.size()), solution.begin() + ((second_edge_start_index + 1) % solution.size()));
 
-                //std::cout << "[imp " << best_improvement << " edge " << solution[first_edge_start_index] << "->" << solution[(first_edge_start_index + 1) % solution.size()];
-                //std::cout << " into " << solution[second_edge_start_index] << "->" << solution[(second_edge_start_index + 1) % solution.size()];
-                //std::cout << " in " << first_edge_start_index << " and " << second_edge_start_index << "], ";
+            //std::cout << "[imp " << best_improvement << " edge " << solution[first_edge_start_index] << "->" << solution[(first_edge_start_index + 1) % solution.size()];
+            //std::cout << " into " << solution[second_edge_start_index] << "->" << solution[(second_edge_start_index + 1) % solution.size()];
+            //std::cout << " in " << first_edge_start_index << " and " << second_edge_start_index << "], ";
                     
                 
+        }
+        else { // No improvement, break the local search loop, local optimum found!
+            return solution;
+        }
+        
+    }
+
+    return solution;
+}
+
+
+std::vector <int> greedy_solution(std::vector <int> solution, std::vector <std::vector <int>>& edge_matrix) {
+    // if not steepest_neighborhood, then greedy
+    // if not edges_exchange, then nodes exchange // But intra- and inter- should both be used
+
+
+
+
+    // 100 iterations instead of while(true)
+    for (int t = 0; t < 1000; t++) {
+        int best_improvement = 0;
+        int new_node_index = -1;
+        int replacing_node_index = -1;
+
+        int first_edge_start_index = -1; // By definition the end node will be (i+1 mod size)
+        int second_edge_start_index = -1;
+
+        int first_to_switch_index = -1;
+        int second_to_switch_index = -1;
+
+        int random_offset = rand() % solution.size(); //TODO FIX
+        //int io = (i + random_offset) % solution.size(); // that's for greedy
+
+        int i_old_node_iterating = 0;
+        int i_old_node = (i_old_node_iterating + random_offset) % solution.size();
+        int j_new_node = 0;
+        int old_cost_new_node = 0;
+        bool new_node_finished = false;
+
+        int i_intra_iter = 0; // This calculates when the loop should end
+        int i_intra = (i_intra_iter + random_offset) % solution.size(); // This is used as an index in calculations, and is always offset from i_intra_iter by set random amount
+        int j_intra = 0;
+        bool intra_finished = false;
+
+        // Similar to steepest, but it needs to randomize whether it tries to add a new node or do an internal swap
+        while (true) {
+            // New node advance
+
+            //std::cout << j_intra << "\n";
+
+            if (!intra_finished) {
+                j_intra++;
+                if (j_intra + 1 >= i_intra) {
+                    j_intra = 0;
+                    i_intra_iter++;
+                    if (i_intra_iter >= solution.size()) {
+                        intra_finished = true;
+                        continue; // No edge rearrangement will improve the score
+                    }
+
+                    i_intra = (i_intra_iter + random_offset) % solution.size();
+
+                }
+
+                if (j_intra == i_intra || j_intra == (i_intra + 1) % solution.size() || (j_intra + 1) % solution.size() == i_intra) continue;
+                int old_cost = 0;
+                int cost_improvement = 0;
+
+                old_cost += get_cost(solution[i_intra], solution[(i_intra + 1) % solution.size()], edge_matrix);
+                old_cost += get_cost(solution[j_intra], solution[(j_intra + 1) % solution.size()], edge_matrix);
+
+
+                cost_improvement -= get_cost(solution[(j_intra + 1) % solution.size()], solution[(i_intra + 1) % solution.size()], edge_matrix);
+                cost_improvement -= get_cost(solution[j_intra], solution[i_intra], edge_matrix);
+
+
+                cost_improvement += old_cost;
+
+
+                if (cost_improvement > 0) {
+                    std::cout << ((j_intra + 1) % solution.size()) << " " << ((i_intra + 1) % solution.size()) << "\n";
+                    std::reverse(solution.begin() + ((j_intra + 1) % solution.size()), solution.begin() + ((i_intra + 1) % solution.size()));
+                    break;
+                }
+
             }
-            else { // No improvement, break the local search loop, local optimum found!
+            else {
+
                 return solution;
+                // No improvement can be found by greedy, so end
             }
         }
-        else {
-            int random_offset = rand() % solution.size(); //TODO FIX
-            //int io = (i + random_offset) % solution.size(); // that's for greedy
-
-            int i_old_node_iterating = 0;
-            int i_old_node = (i_old_node_iterating + random_offset) % solution.size();
-            int j_new_node = 0;
-            int old_cost_new_node = 0;
-            bool new_node_finished = false;
-
-            int i_intra_iter = 0; // This calculates when the loop should end
-            int i_intra = (i_intra_iter + random_offset) % solution.size(); // This is used as an index in calculations, and is always offset from i_intra_iter by set random amount
-            int j_intra = 0;
-            bool intra_finished = false;
-
-            // Similar to steepest, but it needs to randomize whether it tries to add a new node or do an internal swap
-            while (true) {
-
-                // New node advance
-                
-                if (!intra_finished) {
-                    j_intra++;
-                    if (j_intra + 1 >= i_intra) {
-                        j_intra = 0;
-                        i_intra_iter++;
-                        if (i_intra_iter >= solution.size()) {
-                            intra_finished = true;
-                            continue; // No edge rearrangement will improve the score
-                        }
-
-                        i_intra = (i_intra_iter + random_offset) % solution.size();
-
-                    }
-
-                    if (j_intra == i_intra || j_intra == (i_intra + 1) % solution.size() || (j_intra + 1) % solution.size() == i_intra) continue;
-                    int old_cost = 0;
-                    int cost_improvement = 0;
-
-                    old_cost += get_cost(solution[i_intra], solution[(i_intra + 1) % solution.size()], edge_matrix);
-                    old_cost += get_cost(solution[j_intra], solution[(j_intra + 1) % solution.size()], edge_matrix);
-
-
-                    cost_improvement -= get_cost(solution[(j_intra + 1) % solution.size()], solution[(i_intra + 1) % solution.size()], edge_matrix);
-                    cost_improvement -= get_cost(solution[j_intra], solution[i_intra], edge_matrix);
-
-
-                    cost_improvement += old_cost;
-
-
-                    if (cost_improvement > 0) {
-                        std::reverse(solution.begin() + ((j_intra + 1) % solution.size()), solution.begin() + ((i_intra + 1) % solution.size()));
-                        break;
-                    }
-                    
-                }
-                else {
-
-                    return solution;
-                    // No improvement can be found by greedy, so end
-                }
-            }
-        }
+        
     }
 
     return solution;
@@ -272,6 +294,24 @@ std::vector <int> solution_search(int seconds, std::string algorithm, std::vecto
 
             temp_solution = random_solution(dimension, rng);
             temp_solution = steepest_solution(temp_solution, edge_matrix);
+            temp_distance = calculate_distance(temp_solution, edge_matrix);
+
+            if (temp_distance < best_distance) {
+                best_distance = temp_distance;
+                best_solution = temp_solution;
+            }
+
+
+            end = std::chrono::steady_clock::now();
+            elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+        }
+    }
+    else if (algorithm == "greedy") {
+
+        while (elapsed_time < stopping_time) {
+
+            temp_solution = random_solution(dimension, rng);
+            temp_solution = greedy_solution(temp_solution, edge_matrix);
             temp_distance = calculate_distance(temp_solution, edge_matrix);
 
             if (temp_distance < best_distance) {
